34514adf17ad9a7fdd20a1dd3495bfa11b9f53dc
DCK <dck@DCKdeMacBook-Air.local>
2026-07-15 02:55:11 +0800

    fix: restore GitHub Pages domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "zh-CN",
-    baseUrl: "narog.cn",
+    baseUrl: "narroog.github.io",
     ignorePatterns: ["private", "templates", "99-Templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
